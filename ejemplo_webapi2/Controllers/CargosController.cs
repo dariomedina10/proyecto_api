a6d6b32fc75343b1fddec6f95d02f696d484bfe0
unknown <dariomedina10@gmail.com>
2023-07-19 01:30:33 -0400

    actualizado crud

--- ejemplo_webapi2/Controllers/CargosController.cs
+++ ejemplo_webapi2/Controllers/CargosController.cs
@@ -1,6 +1,8 @@
 ﻿using Datos;
+using Microsoft.Ajax.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -41,11 +43,61 @@ namespace ejemplo_webapi2.Controllers
         {
             if (ModelState.IsValid)
             {
+                Context.Cargos.Add(cargo);
+                Context.SaveChanges();
+                return Ok(cargo);
 
+            }else {
+
+                return BadRequest();
+            
             }
 
         }
 
+        [HttpPut]
+
+        public IHttpActionResult ActualizarCargo(int id, [FromBody] Cargos cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                var cargoexiste = Context.Cargos.Count(e => e.Departamento_id == id) > 0;
+                if (cargoexiste)
+                {
+                    Context.Entry(cargo).State = EntityState.Modified;
+                    Context.SaveChanges();
+                    return Ok(cargo);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+            
+        }
+
+        [HttpDelete]
+        public IHttpActionResult EliminarCargo(int id) { 
+            var cargo =Context.Cargos.Find(id);
+
+            if (cargo !=null)
+
+            {
+                Context.Cargos.Remove(cargo);
+                Context.SaveChanges();
+                return Ok(cargo);
+            } else
+                {
+                return NotFound();
+            }
+
+
+        }
     }
 
 }
